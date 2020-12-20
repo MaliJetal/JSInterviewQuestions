78bff3b818d05121e3e31733766c65fc1f81963f
Jetal Mali <38008970+MaliJetal@users.noreply.github.com>
2020-12-21 02:48:33 +0530

    Call, Apply, Bind functions
    Call and Apply allows methods to be invoke on any object. Difference is on syntax- Call has comma separated parameter as (ref obj, arg_1, arg_2,..) whereas in apply parameter is array  as ref_obj, [arg_1,arg_2,...])

--- JSFeatures/CallApplyBind.js
+++ JSFeatures/CallApplyBind.js
@@ -1,4 +1,25 @@
-let name = { 
-  fisrtName : "John",
+/****** call,apply band bind function *****/
+
+let name = {
+  firstName: "John",
   lastName: "Doe"
 }
+
+let name2 = {
+  firstName : "Tom",
+  lastName : "Louise"
+}
+
+let printFullName = function(hometown, state) {
+  console.log(this.firstName + " " + this.lastName + " " + "stays at" + " " + hometown + " " + state);
+}
+
+//call() - arg1- reference object, arg2 - n : arguments of tht method
+printFullName.call(name, "Los Angeles", "California");
+
+//apply() - arg1- reference object, arg2 : arguments in array
+printFullName.apply(name2, ["Illinois", "Chicago"]);
+
+//bind() - create a duplicate function and invoke later
+let printName = printFullName.bind(name, "Ohio", "Columbus");
+printName();
