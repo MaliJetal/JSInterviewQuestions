6d058ca95cd5722427cee6e74a828aa371b07c3b
Jetal Mali <38008970+MaliJetal@users.noreply.github.com>
2021-02-20 01:01:33 +0530

    Binary Search tree

--- DataStructures/BinaryTree.js
+++ DataStructures/BinaryTree.js
@@ -1,4 +1,7 @@
 //Binary Tree
+//TC - O(logn)
+//Balanced binary Tree
+//Use Cases - Dictionary, Phonebook, Users of application 
 
 function BST(value){
   this.value = value;
@@ -17,7 +20,7 @@ BST.prototype.insert = function(value){
   }
 }
 
-BST.prototype.constains = function(value){
+BST.prototype.contains = function(value){
   if(value == this.value) return true;
   else if(value <= this.value){
     if(!this.left) return false;
@@ -29,10 +32,43 @@ BST.prototype.constains = function(value){
   }
 }
 
-BST.prototype.depthfirsttraversal = function(iteratorFunc){
-  if(this.left) this.left.depthfirsttraversal(iteratorFunc);
-  iteratorFunc(this.value);
-  if(this.right) this.right.depthfirsttraversal(iteratorFunc);
+BST.prototype.depthfirsttraversal = function(iteratorFunc, order){
+  if(order === 'pre-order') iteratorFunc(this.value);
+  if(this.left) this.left.depthfirsttraversal(iteratorFunc,order);
+  if(order === 'in-order') iteratorFunc(this.value);
+  if(this.right) this.right.depthfirsttraversal(iteratorFunc, order);
+  if(order === 'post-order') iteratorFunc(this.value);
 }
 
-BST.prototype
+BST.prototype.breadthFirstSearch = function(iteratorFunc){
+  var queue = [this];
+  while(queue.length){
+    var treeNode = queue.shift();
+    iteratorFunc(treeNode);
+    if(treeNode.left) queue.push(treeNode.left);
+    if(treeNode.right) queue.push(treeNode.right);
+  }
+}
+
+var bst = new BST(50);
+bst.insert(30);
+bst.insert(20);
+bst.insert(59);
+bst.insert(70);
+bst.insert(10);
+bst.insert(80);
+bst.insert(90);
+bst.insert(100);
+console.log(bst.left.left);
+
+console.log(bst.contains(20));
+
+//bst.depthfirsttraversal(log, 'in-order');
+//bst.depthfirsttraversal(log, 'pre-order');
+//bst.depthfirsttraversal(log, 'post-order');
+
+function log(value){
+  console.log(value.value);
+}
+
+bst.breadthFirstSearch(log);
