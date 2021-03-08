10a9538110d87f3e3033ab276889115581fb3236
Jetal Mali <38008970+MaliJetal@users.noreply.github.com>
2021-03-08 23:36:54 +0530

    Update useEffect

--- React/ReactHooks.js
+++ React/ReactHooks.js
@@ -4,8 +4,8 @@ function Example() {
   // Declare a new state variable, which we'll call "count"
   //useState
   const [count, setCount] = useState(0);
-
-
+  const [isOnline, setIsOnline] = useState(null);
+  
   //useEffect
   // Similar to componentDidMount and componentDidUpdate:
   useEffect(() => {
@@ -13,9 +13,15 @@ function Example() {
       // this component sets the document title after React updates the DOM
       document.title = `You clicked ${count} times.`
     });
-
   
-
+  //useEffect
+    useEffect(() => {
+      ChatAPI.subscribeToFriendStatus(props.friend.id, handleStatusChange);
+      return() => {
+        ChatAPI.unsubscribeToFriendStatus(props.friend.id,handleStatusChange);
+      };
+    });
+  
   function handleStatusChange(status){
     setIsOnline(status.isOnline);
   };
@@ -27,6 +33,14 @@ function Example() {
         onClick = {() => setCount(count+1) }
       >Click me</button>
       
+      <p>
+      { 
+        if(isOnline === null){
+          'Loading ...'
+        }
+        {isOnline} ? 'Online' : 'Offline'
+      }
+      </p>
     </div>
   );
 }
