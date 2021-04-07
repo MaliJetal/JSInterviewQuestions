bb348ae352cf6cb258ba3db09f40013d1340f61c
Jetal Mali <38008970+MaliJetal@users.noreply.github.com>
2021-04-08 01:04:10 +0530

    Create Promises.js

---
+++ Promises.js
@@ -0,0 +1,10 @@
+/*Promises
+Promises lets asynchronous methods to return values at some point of time.
+Promises are used to handles asynchronous functions using handlers with their actions - eventual success or failure.
+
+A Promise is in one of these states:
+
+pending: initial state, neither fulfilled nor rejected. returns  ndefined
+fulfilled: meaning that the operation was completed successfully. returns value
+rejected: meaning that the operation failed. returns error
+*/
